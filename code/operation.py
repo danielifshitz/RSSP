6096e25d97b8b785b3e7b1065dbe5e3eeb8cf3e7
odeliaevgi <odeliaavgi@gmail.com>
2019-01-12 13:20:34 +0200

    add class operation.
    Signed-off-by: odeliaevgi <odeliaavgi@gmail.com>

---
+++ code/operation.py
@@ -0,0 +1,19 @@
+from mode import Mode
+class Operation:
+
+    def __init__(self, num_of_op):
+        self.num_of_op = num_of_op
+        self.modes = []
+        self.global_start_time = -1
+
+    def add_mode_to_operation(self,num_mode,resource,start,dur):
+        for mode in self.modes:
+            if mode.num_mode == num_mode: # mode is exists
+                mode.add_info_to_mode(resource, start, dur)
+                return
+        mode = Mode(num_mode,self.num_of_op)
+        mode.add_info_to_mode(resource, start, dur)
+        self.modes.append(mode)
+
+    def set_global_start_time(self, global_start_time):
+        self.global_start_time = global_start_time
